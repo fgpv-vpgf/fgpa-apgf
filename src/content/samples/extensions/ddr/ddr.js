86c2aeb21f963acfc7938cf714d61b65874627cd
Johann Levesque <johann.levesque@canada.ca>
2018-02-06 12:47:23 -0500

    fix(ddr): Not working in IE11 because of ES6
    Closes #172

--- src/content/samples/extensions/ddr/ddr.js
+++ src/content/samples/extensions/ddr/ddr.js
@@ -1,17 +1,16 @@
-setTimeout(() => {
+setTimeout(function() {
     // add translations
     api.addTranslations('ddrlabel', ['DRR', 'DDR']);
-    api.addTranslations('ddrtooltip', ['Upload to DRR', 'Téléversement au DDR']);
+    api.addTranslations('ddrtooltip', ['Upload to DRR', 'T\él\éversement au DDR']);
 
     // add function to scope
-    scope.ddr = () => {
-        api.setExtensionDialog(`
-            <md-dialog aria-label="{{ 'extensions.ddrtooltip' | translate }}" class="av-extent-dialog">
-                <av-content-panel close-panel="self.close()"
-                        title-style="title" title-value="{{ 'extensions.ddrtooltip' | translate }}">
-                    <av-frame html="./extensions/ddr/frame-ddr.html"></av-frame>
-                </av-content-panel>
-            </md-dialog>`);
+    scope.ddr = function() {
+        api.setExtensionDialog('<md-dialog aria-label="{{ \'extensions.ddrtooltip\' | translate }}" class="av-extent-dialog">' +
+                '<av-content-panel close-panel="self.close()"' +
+                        'title-style="title" title-value="{{ \'extensions.ddrtooltip\' | translate }}">' +
+                    '<av-frame html="./extensions/ddr/frame-ddr.html"></av-frame>' +
+                '</av-content-panel>' +
+            '</md-dialog>');
     }
 
     // add button to the interface
