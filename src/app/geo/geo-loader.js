5a010578615876250ed5ee431e2894f73d535ca2
Pewillia <peter.williams@canada.ca>
2018-03-15 17:41:15 -0400

    fix(addjsdoc): add jsdoc for geo
     closes #206

--- src/app/geo/geo-loader.js
+++ src/app/geo/geo-loader.js
@@ -1,3 +1,11 @@
+/**
+ * @namespace app.geo
+ *
+ * @description
+ *
+ * import the services that expose required functionality of the geoApi
+ *
+ */
 import './gapi.service.js';
 import './layer.service.js';
 import './projection.service.js';
