cc4376355c027e1824dab4613f03bcafb21e3b07
Pewillia <peter.williams@canada.ca>
2018-03-16 11:54:08 -0400

    fix(jsdocaddon) add missing jsdoc  addon
    closes #191

--- src/app/addon/addon-loader.js
+++ src/app/addon/addon-loader.js
@@ -1,4 +1,14 @@
+/**
+ * @namespace app.addon
+ * @description
+ *
+ * import required modules for schema form custom add-on used in app-loader.js
+ */
+
+// module to  set schema form add on sample from a type inside schema.
 import './sampleSchema/sampleSchema.module.js';
+
+// module to set schema form add on sample from a type indside form.
 import './sampleForm/sampleForm.module.js';
 
 // for dynamic select schemaform addon
